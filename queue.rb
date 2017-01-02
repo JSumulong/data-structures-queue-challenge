c666dd9dbd4ee421ee17438add17c5798acef98a
Jsumulong <sumulong28@gmail.com>
2017-01-01 23:11:55 -0600

    write method for #enqueue and #peel, write tests for #enqueue, #peel

--- queue.rb
+++ queue.rb
@@ -1,9 +1,30 @@
+require_relative 'array_list'
+
 class MyQueue
 
 	def initialize
 		@contents = ArrayList.new
 	end
 
+	def enqueue(new_element)
+		contents.add(new_element)
+	end
+
+	def peel
+		contents.get(0)
+	end
+
+	def dequeue
+		# first_item = contents.peel
+		# new_queue = ArrayList.new
+		# i = 1
+		# while i > contents.length
+		# 	new_queue.add(contents)
+		# end
+	end
+
+	private
+	attr_reader :contents
 
 
 end
